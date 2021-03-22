84ceff6fab74f26ff55e85e0b23cec6d7121ace8
Michael Bruno <mikeb@xmos.com>
2021-03-22 16:37:35 -0400

    Cleanup the XMOS demos
    - xmake makefiles replaced with standard GNU makefiles.
    - Removed unused files.

--- FreeRTOS/Demo/XCORE200_XCC/lib_xcore_c/src/xcore_c_interrupt.c
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2016, XMOS Ltd, All rights reserved
-
-#include "xcore_c_interrupt_impl.h"
-
-#include "xcore_c_interrupt.h"
-extern xcore_c_error_t interrupt_mask_all(void);
-extern xcore_c_error_t interrupt_unmask_all(void);
